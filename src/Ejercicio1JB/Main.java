ac688b24af03a616e38d0851c91685c9251b6eb8
Pablo Gustavo Dietrich <pablogustavodietrich@gmail.com>
2023-01-28 14:52:06 -0300

    ejercicio JB 1

---
+++ src/Ejercicio1JB/Main.java
@@ -0,0 +1,33 @@
+package Ejercicio1JB;
+
+public class Main {
+    public static void main(String[] args) {
+       /*Para este primer ejercicio tendréis que realizar lo siguiente:
+      Crea un proyecto de Java desde 0
+      Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+      Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+      Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.*/
+        byte variable1 = 8;
+        short variable2 = 12;
+        int variable3 = 50;
+        long variable4 = 100;
+        boolean variable5 = false;
+        boolean variable6 = true;
+        float variable7 = 8.2f;
+        double variable8 = 12.4;
+        char variable9 = 'a';
+        String variable10 = "hola";
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+        System.out.println(variable9);
+        System.out.println(variable10);
+
+
+    }
+}
